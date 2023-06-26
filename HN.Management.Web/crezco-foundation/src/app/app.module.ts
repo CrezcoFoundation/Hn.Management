65708363b4ab4e7187619450d73c1172cad78762
fcrezco <crezco@outlook.es>
2023-06-26 15:06:58 -0600

    Onboard to ngrx store for give module

--- HN.Management.Web/crezco-foundation/src/app/app.module.ts
+++ HN.Management.Web/crezco-foundation/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,6 +10,9 @@ import { HomeModule } from 'src/home/home.module';
 import { GiveModule } from 'src/give/give.module';
 import { CrezcoStoryModule } from 'src/crezco-story/crezco-story.module';
 import { ContactUsModule } from 'src/contact-us/contact-us.module';
+import { StoreModule } from '@ngrx/store';
+import { crezcoReducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,6 +26,10 @@ import { ContactUsModule } from 'src/contact-us/contact-us.module';
     SharedModule,
     BrowserModule,
     AppRoutingModule,
+    StoreModule.forRoot(crezcoReducers, {
+      metaReducers
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
   ],
 })
 export class AppModule {}
---
+++ HN.Management.Web/crezco-foundation/src/give/actions/give.actions.ts
@@ -0,0 +1,9 @@
+import { createAction, props } from '@ngrx/store';
+import { DonatationAreasInterface } from '../interfaces/donation.interface';
+
+export const fillDonationAreas = createAction(
+    '[give donation areas form] fillDonationAreas', 
+    props<{donationAreas: DonatationAreasInterface}>()
+);
+export const donationareasSummary = createAction('[give donation areas form] donationareasSummary');
+export const reset = createAction('[give donation areas form] Reset');

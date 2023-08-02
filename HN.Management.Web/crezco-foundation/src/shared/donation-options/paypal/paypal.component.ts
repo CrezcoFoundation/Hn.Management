77e87e875b7b3e56fa67131385bd728dc945a2ca
Nerm <nerm.animator@gmail.com>
2023-08-02 15:05:59 -0700

    update payment options of modal

--- HN.Management.Web/crezco-foundation/src/shared/donation-options/paypal/paypal.component.ts
+++ HN.Management.Web/crezco-foundation/src/shared/donation-options/paypal/paypal.component.ts
@@ -14,7 +14,8 @@ export class PaypalComponent implements OnInit {
       hosted_button_id: environment.paypal.hosted_button_id,
       // business: environment.paypal.business,
       image: {
-        src: 'https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif',
+        //src: 'https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif',
+        src: '../../../assets/Image/payment-logos/Yellow_PayPal_Button.png',
         title: 'PayPal - The safer, easier way to pay online!',
         alt: 'Donate with PayPal button',
       },

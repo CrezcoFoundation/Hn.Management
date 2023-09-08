d11273d267dd0eafeb2a0f1637a16522efbd90f4
Nerm <nerm.animator@gmail.com>
2023-09-07 17:11:54 -0700

    contact-us updates

--- HN.Management.Web/crezco-foundation/src/app/app-routing.module.ts
+++ HN.Management.Web/crezco-foundation/src/app/app-routing.module.ts
@@ -10,17 +10,14 @@ const routes: Routes = [
   { path: 'crezco-story', component: CrezcoStoryComponent },
   { path: 'give', component: GiveComponent },
   {
-     path: 'auth',
-      loadChildren: () =>
-    import('../auth/auth.module').then((m) => m.AuthModule),
+    path: 'auth',
+    loadChildren: () => import('../auth/auth.module').then((m) => m.AuthModule),
   },
   {
-     path: 'projects',
-  loadChildren: () =>
-   import('../projects/projects.module').then(
-    (m) => m.ProjectsModule
-  ),
- },
+    path: 'projects',
+    loadChildren: () =>
+      import('../projects/projects.module').then((m) => m.ProjectsModule),
+  },
   { path: '', component: HomeComponent },
 ];
 
--- HN.Management.Web/crezco-foundation/src/contact-us/contact-us.module.ts
+++ HN.Management.Web/crezco-foundation/src/contact-us/contact-us.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ContactUsComponent } from './contact-us/contact-us.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from 'src/shared/shared.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 @NgModule({
   declarations: [ContactUsComponent],
-  imports: [ReactiveFormsModule, SharedModule, CommonModule],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    SharedModule,
+    FormsModule,
+    BrowserModule,
+    SweetAlert2Module,
+  ],
   exports: [ContactUsComponent],
 })
 export class ContactUsModule {}

9c038515e1d72ae8592c9c69e3f8328b3c7b5522
Nerm <nerm.animator@gmail.com>
2023-09-21 18:02:17 -0700

    Some new updates in general site

--- HN.Management.Web/crezco-foundation/src/app/app.component.ts
+++ HN.Management.Web/crezco-foundation/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import Aos from 'aos';
 
 @Component({
@@ -7,9 +8,18 @@ import Aos from 'aos';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
+
   title = 'crezco-foundation';
 
   ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Cuando se completa una navegación, lleva la página al principio.
+        window.scrollTo(0, 0);
+      }
+    });
+
     Aos.init({
       once: false,
       duration: 800,

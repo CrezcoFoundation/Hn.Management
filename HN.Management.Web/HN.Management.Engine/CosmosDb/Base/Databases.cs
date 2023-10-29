bdd48e42f1cc4e033fad5b65df3337ad7e0d320a
jearsoft <jearsoft@gmail.com>
2023-10-29 14:18:37 -0600

    add databases

---
+++ HN.Management.Web/HN.Management.Engine/CosmosDb/Base/Databases.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HN.Management.Engine.CosmosDb.Base
+{
+    public static class Databases
+    {
+        public const string CrezcoDatabaseId = "CrezcoFoundation";
+        public const string CrezcoCollectionName = "CrezcoServices";
+    }
+}

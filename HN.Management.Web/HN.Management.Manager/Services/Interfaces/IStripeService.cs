05c06cd3f362fc4c7ff9ae4f6f751365680f4b4b
Jonathan Andrade <acorea@crezcofoundation.onmicrosoft.com>
2024-06-11 22:46:20 -0600

    add get and update requests

--- HN.Management.Web/HN.Management.Manager/Services/Interfaces/IStripeService.cs
+++ HN.Management.Web/HN.Management.Manager/Services/Interfaces/IStripeService.cs
@@ -7,11 +7,22 @@ namespace HN.Management.Manager.Services.Interfaces
     public interface IStripeService
     {
         Task<CustomerResource> CreateCustomer(CreateCustomerResource resource, CancellationToken cancellationToken);
+        Task<Customer> GetCustomer(string id, CustomerGetOptions resource, CancellationToken cancellationToken);
         Task<CustomerResource> UpdateCustomer(string id, CustomerUpdateOptions resource, CancellationToken cancellationToken);
         Task<Price> CreatePrice(PriceCreateOptions resource, CancellationToken cancellationToken);
         Task<PaymentIntentResponse> CreatePaymentIntent(PaymentIntentCreateOptions resource, CancellationToken cancellationToken);
         Task<Invoice> CreateInvoice(InvoiceCreateOptions resource, CancellationToken cancellationToken);
         Task<Subscription> CreateSubscription(SubscriptionCreateOptions resource, CancellationToken cancellationToken);
         Task<SetupIntent> CreateSetupIntent(SetupIntentCreateOptions resource, CancellationToken cancellationToken);
+        Task<Price> GetPrice(string id, PriceGetOptions getOptions, CancellationToken cancellationToken);
+        Task<Price> UpdatePrice(string id, PriceUpdateOptions resource, CancellationToken cancellationToken);
+        Task<SetupIntent> GetSetupIntent(string id, SetupIntentGetOptions getOptions, CancellationToken cancellationToken);
+        Task<SetupIntent> UpdateSetupIntent(string id, SetupIntentUpdateOptions resource, CancellationToken cancellationToken);
+        Task<PaymentIntent> GetPaymentIntent(string id, PaymentIntentGetOptions getOptions, CancellationToken cancellationToken);
+        Task<PaymentIntent> UpdatePaymentIntent(string id, PaymentIntentUpdateOptions resource, CancellationToken cancellationToken);
+        Task<Invoice> GetInvoice(string id, InvoiceGetOptions getOptions, CancellationToken cancellationToken);
+        Task<Invoice> UpdateInvoice(string id, InvoiceUpdateOptions resource, CancellationToken cancellationToken);
+        Task<Subscription> GetSubscription(string id, SubscriptionGetOptions getOptions, CancellationToken cancellationToken);
+        Task<Subscription> UpdateSubscription(string id, SubscriptionUpdateOptions updateOptions, CancellationToken cancellationToken);
     }
 }
--- HN.Management.Web/HN.Management.Manager/Services/StripeService.cs
+++ HN.Management.Web/HN.Management.Manager/Services/StripeService.cs
@@ -14,7 +14,7 @@ namespace HN.Management.Manager.Services
         private readonly InvoiceService _invoiceService;
         private readonly SubscriptionService _subscriptionService;
         private readonly SetupIntentService _setupIntentService;
-        
+
         public StripeService(CustomerService customerService,
                             PriceService priceService,
                             PaymentIntentService paymentIntentService,
@@ -43,8 +43,13 @@ namespace HN.Management.Manager.Services
             return new CustomerResource(customer.Id, customer.Email, customer.Name);
         }
 
+        public async Task<Customer> GetCustomer(string id, CustomerGetOptions resource, CancellationToken cancellationToken)
+        {
+            return await _customerService.GetAsync(id, resource, null, cancellationToken);
+        }
+
         public async Task<CustomerResource> UpdateCustomer(string id, CustomerUpdateOptions resource, CancellationToken cancellationToken)
-        { 
+        {
             var customer = await _customerService.UpdateAsync(id, resource, null, cancellationToken);
 
             return new CustomerResource(customer.Id, customer.Email, customer.Name);
@@ -52,12 +57,32 @@ namespace HN.Management.Manager.Services
 
         public async Task<Price> CreatePrice(PriceCreateOptions resource, CancellationToken cancellationToken)
         {
-          return await _priceService.CreateAsync(resource, null, cancellationToken); 
+            return await _priceService.CreateAsync(resource, null, cancellationToken);
+        }
+
+        public async Task<Price> GetPrice(string id, PriceGetOptions getOptions, CancellationToken cancellationToken)
+        {
+            return await _priceService.GetAsync(id, getOptions, null, cancellationToken);
+        }
+
+        public async Task<Price> UpdatePrice(string id, PriceUpdateOptions updateOptions, CancellationToken cancellationToken)
+        {
+            return await _priceService.UpdateAsync(id, updateOptions, null, cancellationToken);
         }
 
         public async Task<SetupIntent> CreateSetupIntent(SetupIntentCreateOptions resource, CancellationToken cancellationToken)
         {
             return await _setupIntentService.CreateAsync(resource, null, cancellationToken);
+        }
+
+        public async Task<SetupIntent> GetSetupIntent(string id, SetupIntentGetOptions getOptions, CancellationToken cancellationToken)
+        {
+            return await _setupIntentService.GetAsync(id, getOptions, null, cancellationToken);
+        }
+
+        public async Task<SetupIntent> UpdateSetupIntent(string id, SetupIntentUpdateOptions setupIntentOptions, CancellationToken cancellationToken)
+        {
+            return await _setupIntentService.UpdateAsync(id, setupIntentOptions, null, cancellationToken);
         }
 
         public async Task<PaymentIntentResponse> CreatePaymentIntent(PaymentIntentCreateOptions resource, CancellationToken cancellationToken)
@@ -71,14 +96,44 @@ namespace HN.Management.Manager.Services
                 paymentIntent.Currency);
         }
 
+        public async Task<PaymentIntent> GetPaymentIntent(string id, PaymentIntentGetOptions getOptions, CancellationToken cancellationToken)
+        {
+            return await _paymentIntentService.GetAsync(id, getOptions, null, cancellationToken);
+        }
+
+        public async Task<PaymentIntent> UpdatePaymentIntent(string id, PaymentIntentUpdateOptions updateOptions, CancellationToken cancellationToken)
+        {
+            return await _paymentIntentService.UpdateAsync(id, updateOptions, null, cancellationToken);
+        }
+
         public async Task<Invoice> CreateInvoice(InvoiceCreateOptions resource, CancellationToken cancellationToken)
         {
             return await _invoiceService.CreateAsync(resource, null, cancellationToken);
         }
 
+        public async Task<Invoice> GetInvoice(string id, InvoiceGetOptions getOptions, CancellationToken cancellationToken)
+        {
+            return await _invoiceService.GetAsync(id, getOptions, null, cancellationToken);
+        }
+
+        public async Task<Invoice> UpdateInvoice(string id, InvoiceUpdateOptions updateOptions, CancellationToken cancellationToken)
+        {
+            return await _invoiceService.UpdateAsync(id, updateOptions, null, cancellationToken);
+        }
+
         public async Task<Subscription> CreateSubscription(SubscriptionCreateOptions resource, CancellationToken cancellationToken)
         {
             return await _subscriptionService.CreateAsync(resource, null, cancellationToken);
+        }
+
+        public async Task<Subscription> GetSubscription(string id, SubscriptionGetOptions getOptions, CancellationToken cancellationToken)
+        {
+            return await _subscriptionService.GetAsync(id, getOptions, null, cancellationToken);
+        }
+
+        public async Task<Subscription> UpdateSubscription(string id, SubscriptionUpdateOptions updateOptions, CancellationToken cancellationToken)
+        {
+            return await _subscriptionService.UpdateAsync(id, updateOptions, null, cancellationToken);
         }
     }
 }

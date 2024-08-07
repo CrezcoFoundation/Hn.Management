8985522055324168047f18daf1d0acbb8191f1a7
jestrada <jafestrada21@gmail.com>
2024-08-07 13:45:07 -0700

    update paypal button id and prod script

--- HN.Management.Web/HN.Management.Web/ClientApp/src/environments/environment.prod.ts
+++ HN.Management.Web/HN.Management.Web/ClientApp/src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   paypal: {
     env: 'prod',
-    hosted_button_id: 'TBZXER826VLHC',
+    hosted_button_id: 'R3QWF9WVQ5WSN',
     business: 'acorea@crezcofoundation.org',
-  }, 
+  },
   title: 'Crezco Foundation Org',
   api_url: 'https://www.crezcofoundation.org'
 };

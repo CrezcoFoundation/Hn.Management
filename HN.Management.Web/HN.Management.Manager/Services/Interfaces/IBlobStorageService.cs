dac5bb8bfe5437d32cf8c9dcfa09c469881f8ef8
Jonathan Andrade <acorea@crezcofoundation.onmicrosoft.com>
2024-09-01 16:57:34 -0600

    blob storage implementation, and user updates

---
+++ HN.Management.Web/HN.Management.Manager/Services/Interfaces/IBlobStorageService.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace HN.Management.Manager.Services.Interfaces
+{
+    public interface IBlobStorageService
+    {
+        Task<string> DownloadBlobAsync(string blobName);
+        Task UploadBlobAsync(Stream fileStream, string blobName);
+        string GenerateSasToken(string blobName);
+        string GetMediaUrl(string blobName);
+    }
+}
